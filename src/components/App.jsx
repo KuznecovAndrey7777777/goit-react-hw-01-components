fbe7f14bff0f28ae299de8f7fc08366b8927c418
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2023-12-22 14:58:49 +0200

    save2212231458

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import Profile from './Profile/Profile';
+import data from './user.json';
+import Statistics from './Statistics/Statistics';
+import stats from './data';
+import FriendList from './FriendList/FriendList';
+import friends from './friends.json';
+// import Transactions from './Transactions/Transactions';
+// import transaction from './transactions.json';
+
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={data.username}
+        tag={data.tag}
+        location={data.location}
+        avatar={data.avatar}
+        stats={data.stats}
+      />
+      <Statistics stats={stats} />
+      <FriendList friends={friends} />
     </div>
   );
-};
+}
+
+/* <Transactions items={transaction} /> */
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import styles from './FriendList.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.item}>
+      <span className={isOnline ? styles.statusOnline : styles.statusOffline} />
+      <img className={styles.avatar} src={avatar} alt={`Аватар ${name}`} />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
+
